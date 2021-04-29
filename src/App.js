435d12e5cbaff282fc4c920bc1a3971528bd6b88
Johnatan Fallen <johnatan.f100@gmail.com>
2021-04-29 10:02:20 +0700

    create Header component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Header from './components/Header'
 import Navigation from './components/Navigation'
 import './css/style.css'
 
@@ -6,6 +7,7 @@ const App = () => {
   return (
     <>
       <Navigation />
+      <Header />
     </>
   );
 }
